03b64168d77e7878981cef9b9afa458013f08a32
Jhuder Palomino <jhuder.palomino.27@unsch.edu.pe>
2021-06-28 15:27:07 -0500

    rutas de log

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 const express = require('express');
-const path =require('path');
+const path = require('path');
 const exphbs = require('express-handlebars');
-const methodOverride= require('method-override');
+const methodOverride = require('method-override');
 const session = require('express-session');
 
 //Inicializations
-const app= express();
-//require('./database');
+const app = express();
+require('./database');
+
 
 // Settings
 app.set('port', process.env.PORT || 3000);
-app.set('views',path.join(__dirname,'views'));
-app.engine('.hbs',exphbs({
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
     defaultLayout: 'main',
-    layoutsDir: path.join(app.get('views'),'layouts'),
-    partialsDir: path.join(app.get('views'),'partials'),
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
     extname: '.hbs'
 
 }));
-app.set('view engine','.hbs');
+app.set('view engine', '.hbs');
 
 // Middlewares
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(session({
     secret: 'mysecretapp',
@@ -30,16 +31,34 @@ app.use(session({
 }));
 
 // Global Variables
-
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
 // Routes
 app.use(require('./routes/index'));
 app.use(require('./routes/notes'));
 app.use(require('./routes/users'));
 
 // Static Files
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Server is listening
-app.listen(app.get('port'),() => {
+app.listen(app.get('port'), () => {
     console.log('Server on port ', app.get('port'));
 });
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/users/signin', (req, res) =>{
+router.get('/users/signin', (req, res) => {
     res.render('users/signin');
 });
-router.get('/users/signup', (req, res) =>{
+router.get('/users/signup', (req, res) => {
     res.render('users/signup');
 });
-module.exports= router;
+router.get('/users/recovery', (req, res) => {
+    res.render('users/recovery');
+});
+module.exports = router;
